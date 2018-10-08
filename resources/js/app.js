6cc460217d38b49963471f70a65604f74d006dcb
Doi-Nguyen <vandoi2210@gmail.com>
2018-10-09 00:13:29 +0700

    install firebase

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,11 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+// require firebase
+window.VueFire = require('vuefire');
+window.FireBase = require('firebase');
+
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -17,6 +22,14 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
+var config = {
+    apiKey: "AIzaSyCuCy_Zsyz55t5u_OFksDccO2OkrOoGT9o",
+    authDomain: "app-chat-59eb3.firebaseapp.com",
+    databaseURL: "https://app-chat-59eb3.firebaseio.com",
+    storageBucket: "app-chat-59eb3.appspot.com"
+};
+FireBase.initializeApp(config);
+var database = FireBase.database();
 const app = new Vue({
     el: '#app'
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div id="app-chat" class="app-chat py-2">
+<div id="app-chat" class="app-chat">
     <div class="container">
-        <div class="app-main d-flex">
-            <div class="app-sidebar box-shadow-4dx">
+        <div class="app-main d-flex box-shadow-4dx">
+            <div class="app-sidebar">
                 <div class="app-search py-4 px-2">
                     <div class="input-group">
                         <input type="search" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="marco-addon">
@@ -16,7 +16,7 @@
                 <div class="sidebar-main">
                     <div class="user-list app-scrollbar">
                         <div class="user-item active">
-                            <div class="card flex-row p-4">
+                            <div class="card flex-row px-4 py-3">
                                 <div class="container-img user-avatar app-avatar mr-4">
                                     <img src="{{asset('/images/avatar_01.jpg')}}" alt="user">
                                 </div>
@@ -24,18 +24,13 @@
                                     <div class="user-item-heading d-flex justify-content-between align-items-center">
                                         <span class="user-name">Elizabeth Elliott</span>
                                         <span class="badge badge-info">4</span>
-                                        {{--<span class="user-time">4:14 AM</span>--}}
                                     </div>
-                                    {{--<div class="user-item-text d-flex justify-content-between">--}}
-                                        {{--<span class="message-sent"><i class=""></i> Okay</span>--}}
-                                        {{--<span class="message-number">12</span>--}}
-                                    {{--</div>--}}
                                 </div>
                             </div>
                         </div>
 
                         <div class="user-item">
-                            <div class="card flex-row p-4">
+                            <div class="card flex-row px-4 py-3">
                                 <div class="container-img user-avatar app-avatar mr-4">
                                     <img src="{{asset('/images/avatar_01.jpg')}}" alt="user">
                                 </div>
@@ -48,7 +43,7 @@
                         </div>
 
                         <div class="user-item">
-                            <div class="card flex-row p-4">
+                            <div class="card flex-row px-4 py-3">
                                 <div class="container-img user-avatar app-avatar mr-4">
                                     <img src="{{asset('/images/avatar_01.jpg')}}" alt="user">
                                 </div>
@@ -60,7 +55,7 @@
                             </div>
                         </div>
                         <div class="user-item">
-                            <div class="card flex-row p-4">
+                            <div class="card flex-row px-4 py-3">
                                 <div class="container-img user-avatar app-avatar mr-4">
                                     <img src="{{asset('/images/avatar_01.jpg')}}" alt="user">
                                 </div>
@@ -72,7 +67,7 @@
                             </div>
                         </div>
                         <div class="user-item">
-                            <div class="card flex-row p-4">
+                            <div class="card flex-row px-4 py-3">
                                 <div class="container-img user-avatar app-avatar mr-4">
                                     <img src="{{asset('/images/avatar_01.jpg')}}" alt="user">
                                 </div>
@@ -84,7 +79,7 @@
                             </div>
                         </div>
                         <div class="user-item">
-                            <div class="card flex-row p-4">
+                            <div class="card flex-row px-4 py-3">
                                 <div class="container-img user-avatar app-avatar mr-4">
                                     <img src="{{asset('/images/avatar_01.jpg')}}" alt="user">
                                 </div>
@@ -96,7 +91,7 @@
                             </div>
                         </div>
                         <div class="user-item">
-                            <div class="card flex-row p-4">
+                            <div class="card flex-row px-4 py-3">
                                 <div class="container-img user-avatar app-avatar mr-4">
                                     <img src="{{asset('/images/avatar_01.jpg')}}" alt="user">
                                 </div>
@@ -107,29 +102,11 @@
                                 </div>
                             </div>
                         </div>
-
-                        {{--<div class="user-item">--}}
-                            {{--<div class="card flex-row p-4">--}}
-                                {{--<div class="container-img user-avatar app-avatar mr-4">--}}
-                                    {{--<img src="{{asset('/images/avatar_01.jpg')}}" alt="user">--}}
-                                {{--</div>--}}
-                                {{--<div class="card-body p-0">--}}
-                                    {{--<div class="user-item-heading d-flex justify-content-between mb-2">--}}
-                                        {{--<span class="user-name">Elizabeth Elliott</span>--}}
-                                        {{--<span class="user-time">4:14 AM</span>--}}
-                                    {{--</div>--}}
-                                    {{--<div class="user-item-text d-flex justify-content-between">--}}
-                                        {{--<span class="message-sent"><i class=""></i> Okay</span>--}}
-                                        {{--<span class="message-number">12</span>--}}
-                                    {{--</div>--}}
-                                {{--</div>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
                     </div>
                 </div>
             </div>
-            <div class="app-layout box-shadow-4dx">
-                <div class="app-layout-tool">
+            <div class="app-layout">
+                <div class="app-layout-item app-layout-tool">
                     <div class="layout-tool py-3 px-3">
                         <div class="user-item">
                             <div class="card flex-row border-0">
@@ -146,13 +123,13 @@
                         </div>
                     </div>
                 </div>
-                <div class="app-layout-main parallax">
-                    <div class="app-layout-chat">
+                <div class="app-layout-item app-layout-main parallax">
+                    <div class="app-layout-chat app-scrollbar">
                         <div class="layout-chat">
                             <div class="chat-history text-center py-4 px-2">
                                 <span class="badge badge-dark badge-secondary">Chat History</span>
                             </div>
-                            <div class="chats-main app-scrollbar">
+                            <div class="chats-main">
                                 <div class="chats">
                                     <div class="chat chat-not-mine">
                                         <div class="chat-avatar container-img app-avatar">
@@ -207,7 +184,24 @@
                             </div>
                         </div>
                     </div>
-                    <div class="app-layout-typing"></div>
+                </div>
+                <div class="app-layout-item app-layout-typing">
+                    <div class="layout-typing px-2 py-2">
+                        <div class="type-message">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">$</span>
+                                </div>
+                                <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)" placeholder="Message">
+                                <div class="input-group-append">
+                                    <span class="input-group-text">.00</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="send-message">
+                            <button class="marco-btn btn btn-primary">Send</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
